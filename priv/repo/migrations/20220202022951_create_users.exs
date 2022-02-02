50f6215d955d8e3c93a4d6e8c8405e1e00a4952e
Barbara Jaber <bpolianajaber@gmail.com>
2022-02-01 23:53:01 -0300

    feat: add users migration

---
+++ priv/repo/migrations/20220202022951_create_users.exs
@@ -0,0 +1,15 @@
+defmodule ScorerApi.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def up do
+    create table(:users) do
+      add :points, :integer, null: false
+
+      timestamps()
+    end
+  end
+
+  def down do
+    drop table(:users)
+  end
+end
