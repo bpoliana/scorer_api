8ac50f03137d4944ea3b272cabb8506d2c838288
Barbara Jaber <bpolianajaber@gmail.com>
2022-02-02 01:09:17 -0300

    feat: add get_user_by_points method

--- lib/scorer_api/users.ex
+++ lib/scorer_api/users.ex
@@ -6,7 +6,6 @@ defmodule ScorerApi.Users do
   import Ecto.Query, warn: false
   alias ScorerApi.{Repo, Users.User}
 
-  @spec create_user()
   def create_user(params \\ %{}) do
     %User{}
     |> User.changeset(params)
@@ -21,5 +20,17 @@ defmodule ScorerApi.Users do
 
   def list_users do
     Repo.all(User)
+  end
+
+  def get_user_by_points(points) do
+    query =
+      from(
+        user in User,
+        where: user.points >= ^points,
+        order_by: [:points],
+        limit: 1
+      )
+
+    Repo.one(query)
   end
 end
