27bd3847ff1e3f45753b3b315534c4ac973683f9
Barbara Jaber <bpolianajaber@gmail.com>
2022-02-08 00:01:37 -0300

    test: returns a list with one user

--- test/scorer_api_web/controllers/user_controller_test.exs
+++ test/scorer_api_web/controllers/user_controller_test.exs
@@ -4,14 +4,27 @@ defmodule ScorerApiWeb.UserControllerTest do
 
   import ScorerApi.Factory
 
-  describe "index" do
-    test "should list an empty list of users", %{conn: conn} do
+  describe "index/2" do
+    test "returns an empty list of users", %{conn: conn} do
+      user = insert!(:user, %{points: 99})
+
       response =
         conn
         |> get(Routes.user_path(conn, :index))
         |> json_response(200)
 
-      assert response == %{"data" => []}
+      assert response == %{"data" => [%{"id" => user.id, "points" => user.points}]}
+    end
+
+    test "returns a list with one user", %{conn: conn} do
+      user = insert!(:user, %{points: 99})
+
+      response =
+        conn
+        |> get(Routes.user_path(conn, :index))
+        |> json_response(200)
+
+      assert response == %{"data" => [%{"id" => user.id, "points" => user.points}]}
     end
   end
 end
