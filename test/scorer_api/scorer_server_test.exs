e9be85b4574d207b9858c7799d1c6defd722f200
Barbara Jaber <bpolianajaber@gmail.com>
2022-02-19 19:53:32 -0300

    refactor: scorer server tests with a valid context

--- test/scorer_api/scorer_server_test.exs
+++ test/scorer_api/scorer_server_test.exs
@@ -1,17 +1,17 @@
 defmodule ScorerApi.ScorerServerTest do
   use ScorerApi.DataCase, async: false
 
-  import Mox
+  import ScorerApi.Factory
 
   alias ScorerApi.Servers.ScorerServer
 
+  setup_all do
+    start_supervised(ScorerServer)
+
+    :ok
+  end
+
   describe "start_link/0" do
-    setup do
-      start_supervised(ScorerServer)
-
-      :ok
-    end
-
     test "returns :already_started when initialized again" do
       assert {:error, {:already_started, pid}} = ScorerServer.start_link(nil)
       assert is_pid(pid)
@@ -19,8 +19,16 @@ defmodule ScorerApi.ScorerServerTest do
   end
 
   describe "get_users/0" do
-    test "returns a list of users and nil timestamp with first call" do
+    test "returns a list with no user and nil timestamp in first call and at most 2 users with 100 points call timestamp on second call" do
       assert %{timestamp: nil, users: []} == ScorerServer.get_users()
+
+      user1 = insert!(:user, points: 100)
+      user2 = insert!(:user, points: 100)
+      user3 = insert!(:user, points: 100)
+      call_timestamp = now()
+
+      assert %{timestamp: call_timestamp, users: [user1, user2]} ==
+               ScorerServer.get_users()
     end
   end
 
